7ce1bd54f02a138cf4fadc71fa5edb4c271876c2
DiamondPixel4k <asdfjklasdfjkl26@gmail.com>
2023-06-01 18:47:30 +0100

    added game.cpp

--- game.cpp
+++ game.cpp
@@ -1,41 +1,69 @@
 #include <ncurses.h>
 #include <unistd.h>
+#include <string>
 
-#define DELAY 50000
+#define DELAY 0000
 
 using namespace std;
+
+void erase(int y, int x) {
+	mvaddch(y,x,' ');
+}
+
 
 void setup() {
 	initscr();
 	noecho();
+	cbreak();
+	keypad(stdscr, true);
 	curs_set(FALSE);
 }
 
 int main(int argc, char *argv[]) {
-	int x = 0, y = 0;
-	int maxX = 0, maxY = 0;
-	int nextX = 0;
-	int direction = 1;
-
 	setup();
+	int ch;
+	int next;
+	int y = 0, x = 0;
+	int maxY = 0, maxX = 0;
+	string player = "O";
 
-
-	while(1) {
-		getmaxyx(stdscr,maxY,maxX);
-
-		clear();
-		mvprintw(y,x,"o");
-		refresh();
-
-		usleep(DELAY);
-		nextX = x + direction;
-
-		if (nextX >= maxX || nextX < 0) {
-			direction *= -1;
+	mvprintw(y, x, player.c_str());
+	refresh();
+	while(true) {
+		getmaxyx(stdscr, maxY, maxX);
+		ch = getch();
+		next = 0;
+		if(ch == KEY_LEFT) {
+			next = x - 1;
+			if(next < 0) {continue;}
+			erase(y,x);
+			x -= 1;
+			mvprintw(y,x,player.c_str());
 		}
-		else {x += direction;}
+		if(ch == KEY_RIGHT) {
+			next = x + 1;
+			if(next > maxX - 1) {continue;}
+			erase(y,x);
+			x += 1;
+			mvprintw(y,x,player.c_str());
+		}
+		if(ch == KEY_DOWN) {
+			next = y + 1;
+			if(next > maxY - 1) {continue;}
+			erase(y,x);
+			y += 1;
+			mvprintw(y,x,player.c_str());
+		}
+		if(ch == KEY_UP) {
+			next = y - 1;
+			if(next < 0) {continue;}
+			erase(y,x);
+			y -= 1;
+			mvprintw(y,x,player.c_str());
+		}
+		refresh();
+		usleep(DELAY);
 	}
-
 	endwin();
 
 	return 0;
